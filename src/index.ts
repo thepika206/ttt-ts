9b982fa54d53478eb20a1a908af9aa4e779aabed
pcheung <pika206@gmail.com>
2022-12-10 20:09:26 -0600

    initial html and css

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,37 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+console.log('hello')
 
-import confetti from 'canvas-confetti';
+//Constants========================================================
+const winningCombos: Number[][] = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [6,4,2],
+]
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+// type BoardArray = [number, ...board(number | null)[]]
+type nullNum = number | null
+type winLoseTie = number | 'T' | null
+
+//Variables========================================================
+let board: nullNum [] =[
+  0,
+  1,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null
+]
+
+let turn: nullNum = 0
+let winner: winLoseTie = null
+let finalCombo: Number[]
+//Cached Element References========================================
+//Event Listeners==================================================
+//Functions========================================================
