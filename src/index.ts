bf2155f88f32f7f3361271507b5799bd5daae1fe
pcheung <pika206@gmail.com>
2022-12-10 21:14:04 -0600

    imp event listeners on board and print moves to the console

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-console.log('hello')
+// console.log('hello')
 
 //Constants========================================================
-const winningCombos: Number[][] = [
+const winningCombos: number[][] = [
   [0,1,2],
   [3,4,5],
   [6,7,8],
@@ -13,12 +13,13 @@ const winningCombos: Number[][] = [
 ]
 
 // type BoardArray = [number, ...board(number | null)[]]
-type nullNum = number | null
-type winLoseTie = number | 'T' | null
+type NullNum = -1 | 1 | null
+type WinLoseTie = -1 | 1 | 'T' | null
+type Turn = -1 | 1 //Player O is -1 and Player X is 1
 
 //Variables========================================================
-let board: nullNum [] =[
-  0,
+let board: NullNum [] =[
+  -1,
   1,
   null,
   null,
@@ -29,9 +30,71 @@ let board: nullNum [] =[
   null
 ]
 
-let turn: nullNum = 0
-let winner: winLoseTie = null
-let finalCombo: Number[]
+let turn: Turn
+let winner: WinLoseTie = null
+let finalCombo: NullNum
 //Cached Element References========================================
+const messageEl = document.querySelector<HTMLElement>('#message')
+const resetBtn = document.querySelector<HTMLButtonElement>('#reset')
+const squareEls = document.querySelector<HTMLElement>('main.board')
 //Event Listeners==================================================
+squareEls?.addEventListener('click', function(evt){
+  const move = (evt.target as HTMLElement).id
+  if (!winner && move) {
+    let sqIdx:number = parseInt(move.charAt(2))
+    handleClickBoard(sqIdx)
+  }
+
+})
+resetBtn?.addEventListener('click', init)
+
 //Functions========================================================
+init()
+function init (){
+  board = [
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+  ]
+  turn = 1
+  winner = null
+  finalCombo = null
+  console.log(board)
+  render()
+}
+
+function handleClickBoard(sqIdx:number){
+  if (board[sqIdx]!==null) return //ignore clicks on occupied squares
+  console.log(sqIdx)
+  board[sqIdx] = turn //set the board position according to the current player turn
+  turn *= -1
+  console.log('board',board, 'turn', turn)
+  render()
+}
+
+function render(){
+  renderMessage()
+}
+
+function renderMessage(){
+  let messageText =''
+  if (winner === 'T') {
+    messageText = `No empty spaces remain, looks like a tie game`
+  } else if (winner === null) {
+    let player = turn === -1 ? 'O' : 'X' 
+    messageText = `It's player ${player}'s turn: click any open space` 
+  } else {
+    let winningPlayer = winner -1 ? 'O' : 'X' 
+    messageText = `The winner is ${winningPlayer}!!`
+  } 
+  if (messageEl) messageEl.textContent = messageText
+  // if (finalCombo !== null) renderFinal()
+}
+
+export{}
